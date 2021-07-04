c11099e4dfa66637344717c75a3f25082448c3d9
Marlon Aleman <mjaleman1997@gmail.com>
2021-07-03 23:15:39 -0700

    Add UserRepository.kt

--- app/src/main/java/com/projectmalthorn/roomdemoapp/data/UserRepository.kt
+++ app/src/main/java/com/projectmalthorn/roomdemoapp/data/UserRepository.kt
@@ -1,4 +1,12 @@
 package com.projectmalthorn.roomdemoapp.data
 
-class UserRepository {
+import androidx.lifecycle.LiveData
+
+class UserRepository(private val userDao: UserDao) {
+
+    val readAllData: LiveData<List<User>> = userDao.readAllData()
+
+    suspend fun addUser(user: User){
+        userDao.addUser(user)
+    }
 }
