8673a5603736a5eda7973583857e29094b7da80a
Marlon Aleman <mjaleman1997@gmail.com>
2021-07-03 22:53:46 -0700

    Add Entity class

--- app/src/main/java/com/projectmalthorn/roomdemoapp/data/User.kt
+++ app/src/main/java/com/projectmalthorn/roomdemoapp/data/User.kt
@@ -1,4 +1,13 @@
 package com.projectmalthorn.roomdemoapp.data
 
-data class User {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user_table")
+data class User(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val firstName: String,
+    val lastName: String,
+    val age: Int
+)
