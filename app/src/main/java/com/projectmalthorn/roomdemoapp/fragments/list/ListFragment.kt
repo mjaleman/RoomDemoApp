f4420d356e574947777e3efeab917f8aab35ede8
Marlon Aleman <mjaleman1997@gmail.com>
2021-07-05 23:55:24 -0700

    Create layout views.

--- app/src/main/java/com/projectmalthorn/roomdemoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/projectmalthorn/roomdemoapp/fragments/list/ListFragment.kt
@@ -1,20 +1,20 @@
-package com.projectmalthorn.roomdemoapp
+package com.projectmalthorn.roomdemoapp.fragments.list
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.projectmalthorn.roomdemoapp.R
 
 
-class AddFragment : Fragment() {
-
+class ListFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
 }
